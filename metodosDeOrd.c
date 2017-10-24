7f9b0a17f1b91535b84b4bec670514c2c3f3ee1c
MarcioJrios <31110984+MarcioJrios@users.noreply.github.com>
2017-10-24 17:33:57 -0200

    Update metodosDeOrd.c
    Bubble Sort, Selection Sort e Insertion Sort.

--- metodosDeOrd.c
+++ metodosDeOrd.c
@@ -1,15 +1,17 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 void bubbleSort(int v[6]){
-	int i;
-	for(i = 0; i<5; i++){
+	int i, temp;
+	for(i=0;i<5;i++){
 		if(v[i]>v[i+1]){
-			int l = v[i+1];
-			v[i+1] = v[i];
-			v[i] = l;
-			i = -1;
+			temp=v[i+1];
+			v[i+1]=v[i];
+			v[i]=temp;
+			i=-1;
 		}
 	}
+	system("clear");
 }
 
 void selectionSort(int v[6]){
@@ -17,23 +19,73 @@ void selectionSort(int v[6]){
 	for(i = 0; i<6; i++){
 		for(j = i+1; j<6; j++){
 			if(v[j] < v[i]){
-				int l = v[j];
-				v[j] = v[i];
-				v[i] = l;
+				int l = v[i];
+				v[i] = v[j];
+				v[j] = l;
 			}
+		}
 	}
 }
 
 void insertionSort(int v[6]){
 	int i, j;
-	for(i = 1; i<6 ; i++){
-		for(j = i-1; j<6; j++){
+	for(i=1;i<6;i++){
+		for(j = i; v[j]<v[j-1]; j--){
+			int l = v[j-1];
+			v[j-1] = v[j];
+			v[j] = l;
+			}	
 			
+	}
+	system("clear");
 }
+
+void print(int v[6]){
+	int i;
+	for(i=0;i<6;i++){
+		printf("Posicao %d: %d;\n", i, v[i]);	
+	}
+}
+
 int main(){
-	int v[6] = {4 i, 2 j, 7, 3, 9, 1};
-	
-
-
-
- 
+	int v[6]={4, 2, 7, 3, 9, 1}, n;
+	do{
+		puts("\t\tMENU\n");
+		puts("(1) Para ordenar o vetor por BubbleSort;");
+		puts("(2) Para ordenar o vetor por SelectionSort;");
+		puts("(3) Para ordenar o vetor por InsertionSort;");
+		puts("(4) Para listar os elementos do vetor;");
+		puts("(0) Sair.\n");
+		printf("Digite o numero correspondente a opcao: ");
+		scanf("%d", &n);
+		switch(n){
+			case 1:
+				system("clear");
+				bubbleSort(v);
+				puts("Voce ordenou por BubbleSort!");
+				break;
+			case 2:
+				system("clear");
+				selectionSort(v);
+				puts("Voce ordenou por SelectionSort!");
+				break;
+			case 3:
+				system("clear");
+				insertionSort(v);
+				puts("Voce ordenou por InsertionSort!");
+				break;
+			case 4:
+				system("clear");
+				print(v);
+				break;
+			case 0:
+				system("clear");
+				puts("Voce escolheu por sair! Ate nunca mais!");
+				break;
+			default:
+				system("clear");
+				puts("Opcao invalida!");
+		}
+	}while(n!=0);
+	return 0;
+}
